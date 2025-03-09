b492ce766c10d20ea36df6138e4caa69fca867df
kuzmichit <kuzmichit@gmail.com>
2025-03-09 08:50:34 +0100

    use a random quote

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,11 +1,13 @@
 import { quotes } from './quotes';
+import style from './Quote.module.css';
 
 export const Quote = () => {
-  const { text, author } = quotes[1];
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const { text, author } = quotes[randomIndex];
 
   return (
     <>
-      <blockquote>{text}</blockquote>
+      <blockquote className={style.quote}>{text}</blockquote>
       <cite>- {author}</cite>
     </>
   );
