e953cc7592ac9c33d6a196d14e906fdb0a4e24e1
kuzmichit <kuzmichit@gmail.com>
2025-03-16 10:45:00 +0100

    add use control inputs

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,15 +1,33 @@
 import styles from './Converter.module.css';
+import { useState } from 'react';
 
 export const Converter = () => {
+  const [rub, setRub] = useState(100);
+  const [usd, setUsd] = useState(1);
+
   return (
     <form className={styles.converter}>
       <label>
         <p>Quantita in RUB:</p>
-        <input type="number" name="rub" min="0" step={1} value={1} />
+        <input
+          type="number"
+          name="rub"
+          min="0"
+          step={1}
+          value={rub}
+          onChange={(e) => setRub(e.target.value)}
+        />
       </label>
       <label>
         <p>Quantita in USD:</p>
-        <input type="number" name="usd" min="0" step={1} value={42} />
+        <input
+          type="number"
+          name="rub"
+          min="0"
+          step={1}
+          value={usd}
+          onChange={(e) => setUsd(e.target.value)}
+        />
       </label>
     </form>
   );
--- src/components/Converter/Converter.test.js
+++ src/components/Converter/Converter.test.js
@@ -1,6 +1,10 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import { Converter } from './Converter';
-afterEach(() => cleanup());
+import userEvent from '@testing-library/user-event';
+
+beforeEach(() => {
+  console.clear(); // Pulisce la console prima di ogni test
+});
 
 describe('when rendered', () => {
   it('input should contain an expected rub amount', () => {
@@ -8,14 +12,35 @@ describe('when rendered', () => {
     render(<Converter />);
 
     // Cerca il testo della citazione all'interno del documento
-    const result = screen.getByLabelText(/Quantita in RUB:/);
-    expect(result).toHaveValue(1);
+    expect(screen.getByLabelText(/Quantita in RUB:/)).toHaveValue(100);
   });
 
   it('input should contain an expected usd amount', () => {
     render(<Converter />);
 
     const result = screen.getByLabelText(/Quantita in USD:/);
-    expect(result).toHaveValue(42);
+    expect(result).toHaveValue(1);
+  });
+});
+
+describe('when typed in the rub input', () => {
+  it('should update its value', async () => {
+    render(<Converter />);
+    const input = screen.getByLabelText(/Quantita in RUB:/);
+
+    userEvent.clear(input);
+    await userEvent.type(input, '42');
+    expect(input).toHaveValue(42);
+  });
+});
+
+describe('when typed in the usd input', () => {
+  it('should update its value', async () => {
+    render(<Converter />);
+    const input = screen.getByLabelText(/Quantita in USD:/);
+
+    userEvent.clear(input);
+    await userEvent.type(input, '57');
+    expect(input).toHaveValue(57);
   });
 });
