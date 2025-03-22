d74019118ce056a41ee489d40991e2873bbb1a68
kuzmichit <kuzmichit@gmail.com>
2025-03-22 12:02:12 +0100

    add the render value

--- src/components/Converter/useConverter.test.js
+++ src/components/Converter/useConverter.test.js
@@ -1,18 +1,18 @@
+import { renderHook } from '@testing-library/react';
+import { useConverter } from './useConverter';
+
 const rate = 42;
 const testRubAmount = 100;
-// const testUsdAmount = 100;
 const calculatedUsdAmount = 2.38;
-// const calculatedRubAmount = testUsdAmount * rate;
-
 describe('when rendered', () => {
-  //   it('input should contain an expected rub amount', () => {
-  //     const { result } = renderHook(() => useConverter(testRubAmount, rate));
-  //     expect(result.current.rub).toEqual(testRubAmount);
-  //   });
-  // it('input should contain an expected usd amount', () => {
-  //   const { result } = renderHook(() => useConverter(testRubAmount, rate));
-  //   expect(result.current.usd).toBe(calculatedUsdAmount);
-  // });
+  it('input should contain an expected rub amount', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, rate));
+    expect(result.current.rub).toEqual(testRubAmount);
+  });
+  it('input should contain an expected usd amount', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, rate));
+    expect(result.current.usd).toBe(calculatedUsdAmount);
+  });
 });
 
 describe('quando chiamiamo "updateRub metodo"', () => {
